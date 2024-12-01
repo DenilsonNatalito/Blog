4c2e8c6e6cb64f9eb51529d57d18c59189593e09
Mr Frogg <denilsonnatalito236@gmail[C.com>
2024-12-01 17:30:50 +0200

    initial commit and create the initial structure.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Header from './components/Header';
+import Article from './components/Article';
+
+
+
+function App() {
+  return ( 
+    <>
+    <Header/>
+    <Article/>
+
+    
+    <h1> Meu Blog</h1>
+  </>
+);
+    
+}
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+
+const Header = () =>{
+    return(
+        
+        <h1> Header.js </h1>
+        
+    );
+}
+
+
+export default Header;
